309862c3969a5d3578a38f04c3502b6b068c412f
Klemen Pogacnik <k.pogacnik@iskratel.si>
2021-01-05 16:47:33 +0100

    usable version

--- dictionary.py
+++ dictionary.py
@@ -5,10 +5,12 @@ import pprint
 import random
 
 DEBUG = False
+CHECK_RAND = False
+DICTIONARY_FILE_PATH = "dictionary.txt"
 
 
 def read_dictionary():
-    with open('dictionary.txt') as json_file:
+    with open(DICTIONARY_FILE_PATH) as json_file:
         dictionary_json = json.load(json_file)
         if DEBUG:
             print("********************* JSON DICTIONARY - begin ******************")
@@ -39,22 +41,67 @@ def select_word(dictionary):
 
 
 def show_word(word):
-    print("Selected word = ", word)
+    print("Word:", word['translation'])
+    ii = 1
+    while True:
+        user_input = input("Answer: ")
+        if (user_input == word['foreign']):
+            break
+        print(word['foreign'][0:ii])
+        if (ii >= len(word['foreign'])):
+            break
+
+        ii += 1
+    print("------------------------------------------------------")
+    print("SUCCESS!")
+    print(word['foreign'], " - ", word['translation'])
+    for textToPrint in word['add_text']:
+        print(textToPrint)
+    print("------------------------------------------------------")
+
+    return
+
+
+def check_rand(sizeofdict, nrOfTrys):
+    checkRand = [0 for ii in range(sizeofdict)]
+    for ii in range(nrOfTrys):
+        randValue = random.randint(0, sizeofdict-1)
+        checkRand[randValue] += 1
+    print("Size=", sizeofdict)
+    for ii in range(sizeofdict):
+        print(ii, checkRand[ii])
     return
 
 
 def main():
+    # initilize rand generator
+    random.seed(a=None, version=2)
+
     # read json dictionary
     my_dictionary = read_dictionary()
-    pp = pprint.PrettyPrinter(indent=4)
+
+    # checking random function - debug
+    if CHECK_RAND:
+        check_rand(len(my_dictionary), 10)
+        exit()
+    # print dictionary - debug
     if DEBUG:
         print("********************* MY DICTIONARY - begin *******************")
+        pp = pprint.PrettyPrinter(indent=4)
         pp.pprint(my_dictionary)
         sizeofdict = len(my_dictionary)
         print("Dictionary size = ", sizeofdict)
         print("********************* MY DICTIONARY - end *********************")
+    # main action
+    firstRun = True
     while True:
-        user_input = input("Try another word(Y/N):")
+        if firstRun:
+            print("Let's start!")
+            user_input = 'Y'
+            firstRun = False
+        else:
+            user_input = input("Try another word(Y/N):")
+        print("------------------------------------------------------")
         if ((user_input == 'Y') or (user_input == 'y')):
             selected_word = select_word(my_dictionary)
             show_word(selected_word)
